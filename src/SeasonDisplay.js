47127a63a614ded2538b4c10173c433d41d77b2d
T.K <53352673+taegyu87@users.noreply.github.com>
2019-08-24 20:07:34 -0400

    Initial commit

---
+++ src/SeasonDisplay.js
@@ -0,0 +1,36 @@
+import "./SeasonDisplay.css";
+import React from "react";
+
+const seasonConfig = {
+  summer: {
+    text: "Let's hit the beach!",
+    iconName: "sun"
+  },
+  wwinter: {
+    text: "Burr it is cold!",
+    iconName: "snowflake"
+  }
+};
+
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? "summer" : "winter";
+  } else {
+    return lat > 0 ? "winter" : "summer";
+  }
+};
+
+const SeasonDisplay = props => {
+  const season = getSeason(props.lat, new Date().getMonth());
+  const { text, iconName } = seasonConfig[season]; // {text, iconName}
+
+  return (
+    <div className={`season-display ${season}`}>
+      <i className={`${iconName} icon massive icon-left`} />
+      <h1>{text}</h1>
+      <i className={`${iconName} icon massive icon-right`} />
+    </div>
+  );
+};
+
+export default SeasonDisplay;
---
+++ src/index.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
+
+class App extends Component {
+  state = { lat: null, errorMessage: "" };
+
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      position => this.setState({ lat: position.coords.latitude }),
+      err => this.setState({ errorMessage: err.message })
+    );
+  }
+
+  // React says we have to define render !!
+  render() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage} </div>;
+    }
+    if (!this.state.errorMessage && this.state.lat) {
+      return <SeasonDisplay lat={this.state.lat} />;
+    }
+    return <Spinner message="Please accept location request" />;
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"));
